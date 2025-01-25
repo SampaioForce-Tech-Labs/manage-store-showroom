84a72724e5da01c53a564ee3cee7c0872abf8a9a
Emm4nuell <eduardo.eesf@gmail.com>
2025-01-25 14:36:50 -0300

    [REFACTOR] - Modificar repositorio swagger para openapi

--- src/main/java/br/com/manage/store/infrastructure/swagger/SwaggerConfig.java
+++ src/main/java/br/com/manage/store/infrastructure/swagger/SwaggerConfig.java
@@ -1,38 +1,28 @@
 package br.com.manage.store.infrastructure.swagger;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-
-import java.util.Collections;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-    public Docket docketApi() {
-        return new Docket(DocumentationType.OAS_30)
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(metaData());
-    }
-
-    private ApiInfo metaData() {
-        return new ApiInfo("Management Store Showroom API",
-                "SampaioForce - Developer",
-                "1.0",
-                "Terms of service",
-                new Contact("DEVELOPER IT", "https://github.com/SampaioForce-Tech-Labs", "vinicius@dhouses.com.br"),
-                "License of API",
-                "API license URL",
-                Collections.emptyList());
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Management Store Showroom API")
+                        .version("1.0")
+                        .description("Documentação da API de Gestão de Clientes: Realize operações de CRUD para gerenciar os clientes da loja.")
+                        .contact(new Contact()
+                                .name("DEVELOPER")
+                                .url("https://github.com/SampaioForce-Tech-Labs")
+                                .email("vinicius@dhouses.com.br"))
+                        .license(new License()
+                                .name("API licença URL"))
+                );
     }
 }
